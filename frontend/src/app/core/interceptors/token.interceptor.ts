2a769b7e6848c20fc435ff96cf263f291cb7e311
Eszter Csaki <esztercsaki3@gmail.com>
2022-08-11 11:44:19 +0200

    created token interceptor

---
+++ frontend/src/app/core/interceptors/token.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from '../services/authentication.service';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  constructor(private authenticationService: AuthenticationService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const tokenRequest = request.clone({
+      setHeaders: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.authenticationService.getToken()}`,
+      },
+    });
+    return next.handle(tokenRequest);
+  }
+}
