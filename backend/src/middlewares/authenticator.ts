b96fcb19259c78f7da0172205c3dce74758a0dfa
Eszter Csaki <esztercsaki3@gmail.com>
2022-08-11 11:27:12 +0200

    created authenticator middleware

---
+++ backend/src/middlewares/authenticator.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from 'express';
+import { userRepository } from '../repositories/userRepository';
+import {
+  forbiddenError,
+  unauthorizedError,
+} from '../services/generalErrorService';
+import { jwtService } from '../services/jwtService';
+
+export default async function getTokenFromRequest(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const token = jwtService.getTokenFromRequest(req);
+
+  if (!token) {
+    next(unauthorizedError('user logged out'));
+    return;
+  }
+
+  if (!jwtService.verifyToken(token as string)) {
+    next(forbiddenError('invalid token'));
+    return;
+  }
+
+  const { userId, userName } = jwtService.getTokenPayload(token);
+
+  try {
+    const userData = await userRepository.getUserByName(userName);
+    if (userData.id !== userId) {
+      next(
+        unauthorizedError(
+          'This account does not belong to authenticated player',
+        ),
+      );
+      return;
+    }
+  } catch (err) {
+    next(err);
+    return;
+  }
+  next();
+}
