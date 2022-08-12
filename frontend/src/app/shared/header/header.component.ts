51ce0cbd00a6eea7d72cf81886cf248f35e7a969
Eszter Csaki <esztercsaki3@gmail.com>
2022-08-12 15:55:13 +0200

    logout button created

--- frontend/src/app/shared/header/header.component.ts
+++ frontend/src/app/shared/header/header.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-  constructor(private router: Router) {}
+export class HeaderComponent {
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {}
 
-  ngOnInit(): void {}
-
-  makeQuiz() {
+  makeQuiz(): void {
     this.router.navigate(['/make-quiz']);
+  }
+
+  logout(): void {
+    this.authenticationService.logout();
   }
 }
