97e378947f0044e20e137d5fea4a4c7b52ce66d3
Eszter Csaki <esztercsaki3@gmail.com>
2022-08-15 14:06:18 +0200

    empty path redirects to login

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'quizzes', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: 'quizzes',
     loadChildren: () =>
