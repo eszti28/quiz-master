c21f0598fc87c141a724c144e332b043382e5e44
Eszter Csaki <esztercsaki3@gmail.com>
2022-08-15 17:18:38 +0200

    added more and different css

--- frontend/src/app/features/my-quizzes/my-quizzes.component.ts
+++ frontend/src/app/features/my-quizzes/my-quizzes.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { QuizService } from 'src/app/core/services/quizService';
+import { QuizMainPageViewModel } from 'src/app/shared/models/view/QuizMainPageViewModel';
 
 @Component({
   selector: 'app-my-quizzes',
   templateUrl: './my-quizzes.component.html',
-  styleUrls: ['./my-quizzes.component.scss']
+  styleUrls: ['./my-quizzes.component.scss'],
 })
 export class MyQuizzesComponent implements OnInit {
+  myQuizzes: QuizMainPageViewModel[];
 
-  constructor() { }
+  constructor(private quizService: QuizService) {}
 
   ngOnInit(): void {
+    this.quizService.getQuizzesByUserId().subscribe((x) => {
+      this.myQuizzes = x;
+    });
   }
-
 }
