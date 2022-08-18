23870b397bf0786113e24469bc04366a1d7e889e
Eszter Csaki <esztercsaki3@gmail.com>
2022-08-18 17:17:41 +0200

    unused imports

--- frontend/src/app/features/make-quiz/new-question/new-question.component.spec.ts
+++ frontend/src/app/features/make-quiz/new-question/new-question.component.spec.ts
@@ -1,9 +1,8 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { RouterTestingModule } from '@angular/router/testing';
-import { QuizService } from 'src/app/core/services/quizService';
 
 import { NewQuestionComponent } from './new-question.component';
 
